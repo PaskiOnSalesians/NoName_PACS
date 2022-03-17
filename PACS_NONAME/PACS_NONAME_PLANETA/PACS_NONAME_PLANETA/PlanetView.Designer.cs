2c50b1ec8e094eeae3e5b7ebca3b3c332f963423
DevPaski <71769140+DevPaski@users.noreply.github.com>
2022-03-17 17:01:48 +0100

    Planetes

---
+++ PACS_NONAME/PACS_NONAME_PLANETA/PACS_NONAME_PLANETA/PlanetView.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace PACS_NONAME_PLANETA
+{
+    partial class PlanetView
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_topbar = new System.Windows.Forms.Panel();
+            this.lblExit = new System.Windows.Forms.Label();
+            this.pnl_topbar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_topbar
+            // 
+            this.pnl_topbar.Controls.Add(this.lblExit);
+            this.pnl_topbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_topbar.Location = new System.Drawing.Point(0, 0);
+            this.pnl_topbar.Name = "pnl_topbar";
+            this.pnl_topbar.Size = new System.Drawing.Size(1264, 49);
+            this.pnl_topbar.TabIndex = 0;
+            this.pnl_topbar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnl_topbar_MouseDown);
+            this.pnl_topbar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnl_topbar_MouseMove);
+            this.pnl_topbar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnl_topbar_MouseUp);
+            // 
+            // lblExit
+            // 
+            this.lblExit.AutoSize = true;
+            this.lblExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExit.Location = new System.Drawing.Point(1229, 9);
+            this.lblExit.Name = "lblExit";
+            this.lblExit.Size = new System.Drawing.Size(23, 25);
+            this.lblExit.TabIndex = 0;
+            this.lblExit.Text = "x";
+            this.lblExit.Click += new System.EventHandler(this.lblExit_Click);
+            // 
+            // PlanetView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.pnl_topbar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PlanetView";
+            this.Text = "PACS - NONAME";
+            this.pnl_topbar.ResumeLayout(false);
+            this.pnl_topbar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_topbar;
+        private System.Windows.Forms.Label lblExit;
+    }
+}
+
---
+++ PACS_NONAME/PACS_NONAME_PLANETA/PACS_NONAME_PLANETA/PlanetView.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PACS_NONAME_PLANETA
+{
+    public partial class PlanetView : Form
+    {
+        #region Variables
+        // Dragging Variables
+        bool dragging = false; // Per a comprovar si podem moure la finestra
+        Point startPoint = new Point(0, 0); // Posicio inicial de la finestra
+        #endregion
+
+        public PlanetView()
+        {
+            InitializeComponent();
+        }
+
+        #region Dragging window
+        private void pnl_topbar_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            startPoint = new Point(e.X, e.Y);
+        }
+
+        private void pnl_topbar_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        private void pnl_topbar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this.startPoint.X, p.Y - this.startPoint.Y);
+            }
+        }
+
+        #endregion
+
+        private void lblExit_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Segur que vols sortir?");
+
+        }
+    }
+}
